b682ef9c84cc7ab06e8956296f494e827b9c8976
Mubeen Tariq <mubeentariq35951@gmail.com>
2026-09-09 13:20:24 +0500

    up

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,6 +8,7 @@ const SOCIALS = {
   tiktok: "https://www.tiktok.com/@techfixbymubeen",
   instagram: "https://www.instagram.com/techfix.by.mubeen",
   facebook: "https://www.facebook.com/Techfix-by-Mubeen",
+  
 };
 
 function Footer() {
